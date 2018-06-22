70189e81f2cb8997e905231fef621e8ea62de531
svoboda21 <39490042+svoboda21@users.noreply.github.com>
2018-06-22 13:47:04 +0400

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,6 +1,6 @@
 <?php
     require_once 'function.php';
-    if (empty($_SESSION['test'])&&empty($_SESSION['name'])){
+    if (empty($_SESSION['test'])||empty($_SESSION['name'])){
         http_response_code(403);
         echo 'Вам доступ запрещен';
         die;
