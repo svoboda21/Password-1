057fd60b220d7d9c9b866d5de8d76c4a3315b78a
svoboda21 <39490042+svoboda21@users.noreply.github.com>
2018-06-22 13:47:26 +0400

    Update list.php

--- list.php
+++ list.php
@@ -1,6 +1,6 @@
 <?php
     require_once 'function.php';
-    if (empty($_SESSION['test'])&&empty($_SESSION['passwordadmin'])){
+    if (empty($_SESSION['test'])||empty($_SESSION['passwordadmin'])){
         http_response_code(403);
         echo 'Вам доступ запрещен';
         die;
